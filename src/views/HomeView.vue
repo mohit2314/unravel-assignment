0e9ebfcfa60bf7f38a10c783b3f53d8617ca74b3
Mohit2314 <mohitpatel2314@gmail.com>
2022-08-24 00:56:51 +0530

    project intial structure

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,62 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="page__container">
+    <div class="header__row">
+      <div class="new__budget__cta">New Budget</div>
+      <div class="search__bar">
+        <input type="search" name="search" placeholder="Search..."/>
+      </div>
+    </div>
+    <!-- Main table section -->
+    <div class="main__table__section">
+      <div class="section__header">
+        <span>Active</span><span><i class="light-icon-info-circle"></i></span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  name: "HomeView",
+  components: {},
+};
+</script>
+
+<style lang="scss">
+.page__container {
+  display: flex;
+  flex-direction: column;
+  gap: 28px;
+
+  .header__row {
+    display: flex;
+    justify-content: space-between;
+
+    .new__budget__cta{
+      padding: 8px 36px;
+      background-color: #4AB9D9;
+      color: #fff;
+      border-radius: 4px;
+      cursor: pointer;
+    }
+
+    .search__bar{
+      width: 20%;
+      input{
+        width: 100%;
+        border: 1px solid grey;
+        padding: 6px;
+        border-radius: 4px;
+        outline: none;
+      }
+    }
+
+  }
+
+  .main__table__section {
+    background-color: #f5f7fa;
   }
 }
-</script>
+</style>
